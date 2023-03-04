ad485ab077b0ee26f105835b3d49e1e4e7ce9081
anas nashat <72121781+anasnashat@users.noreply.github.com>
2023-03-04 02:30:37 +0200

    update

--- demo/api.py
+++ demo/api.py
@@ -1,10 +1,12 @@
 from typing import List
 
+from django.shortcuts import get_object_or_404
 from ninja import NinjaAPI
 from .schema import CategorySchema, ProductSchema
 from .models import Category, Product
 
 api = NinjaAPI()
+
 
 #  add category
 @api.post('/add_category')
@@ -12,17 +14,20 @@ def add_category(request, data: CategorySchema):
     cate = Category.objects.create(**data.dict())
     return {'id': cate.id}
 
+
 #  add product
 @api.post('add-product')
 def add_product(request, data: ProductSchema):
     pro = Product.objects.create(**data.dict())
     return {'id': pro.id}
 
+
 # get all category
 @api.get('category/all', response=List[CategorySchema])
 def get_all_category(request):
     cate = Category.objects.all()
     return cate
+
 
 #  get all product
 @api.get('product/all', response=List[ProductSchema])
@@ -31,8 +36,34 @@ def get_all_products(request):
 
     return pro
 
+
 # get product by category
 @api.get('product/all/category/{category_slug}', response=List[ProductSchema])
-def get_product_by_category(request ,category_slug: str):
+def get_product_by_category(request, category_slug: str):
     pro = Product.objects.filter(category__slug=category_slug)
     return pro
+
+
+@api.put('edite/category/{category_id}')
+def update_category(request, category_id: int, data: CategorySchema):
+    category = get_object_or_404(Category, id=category_id)
+    for attr , value in data.dict().items():
+        if value:
+            setattr(category, attr, value)
+
+    category.save()
+
+    return {'success': True}
+
+
+@api.patch('edite/product/{product_id}')
+def update_product(request, product_id:int, data: ProductSchema):
+    product = get_object_or_404(Product, id=product_id)
+    for attr , value in data.dict().items():
+        if value:
+            setattr(Product, attr, value)
+    product.save()
+
+    return {'success': True}
+
+
